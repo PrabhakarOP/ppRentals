968ec219f21249860835ee0f89c4c307e95efcad
Prince Prabhakar <playhorn.pp@gmail.com>
2025-04-04 22:59:44 +0530

    centered search-bar

--- frontend/script/hamburger-menu.js
+++ frontend/script/hamburger-menu.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const mobileMenuButton = document.getElementById('mobile-menu-button');
             const mobileMenu = document.getElementById('mobile-menu');
+            const searchBar = document.getElementById('search-bar');
             
             mobileMenuButton.addEventListener('click', function() {
                 mobileMenu.classList.toggle('hidden');
-                
+                searchBar.classList.toggle('hidden');
+
                 // Toggle between menu and close icons
                 const icon = mobileMenuButton.querySelector('i');
                 if (mobileMenu.classList.contains('hidden')) {
