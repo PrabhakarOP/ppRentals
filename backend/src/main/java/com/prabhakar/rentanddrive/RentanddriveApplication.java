78d7b66294c77b48684f37e010e313707c830915
Prince Prabhakar <playhorn.pp@gmail.com>
2025-04-05 10:43:27 +0530

    started spring project

---
+++ backend/src/main/java/com/prabhakar/rentanddrive/RentanddriveApplication.java
@@ -0,0 +1,13 @@
+package com.prabhakar.rentanddrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentanddriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentanddriveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/prabhakar/rentanddrive/RentanddriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabhakar.rentanddrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentanddriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
