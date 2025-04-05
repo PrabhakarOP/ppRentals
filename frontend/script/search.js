92c847e1ac092651ab797817e32afb7e5c4a45f5
Prince Prabhakar <playhorn.pp@gmail.com>
2025-04-05 10:35:28 +0530

    added profile page

--- frontend/script/search.js
+++ frontend/script/search.js
@@ -2,6 +2,6 @@ const searchBar=document.getElementById('search-bar');
 
 searchBar.addEventListener("keydown",(event)=>{
     if(event.key === "Enter"){
-        window.location.href='html/search-results.html';
+        window.location.href='../html/search-results.html';
     }
 })
