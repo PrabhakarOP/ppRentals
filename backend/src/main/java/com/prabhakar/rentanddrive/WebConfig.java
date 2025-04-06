7605884d40b38ab8b4a411abd2227699c96d0572
Prince Prabhakar <playhorn.pp@gmail.com>
2025-04-06 16:12:14 +0530

    cors configuration

---
+++ backend/src/main/java/com/prabhakar/rentanddrive/WebConfig.java
@@ -0,0 +1,16 @@
+package com.prabhakar.rentanddrive;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // For development only! Restrict this in production
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
