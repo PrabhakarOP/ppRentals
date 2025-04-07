11d2427d07604da7c65da959f9b6d329618dab65
Prince Prabhakar <playhorn.pp@gmail.com>
2025-04-07 12:27:33 +0530

    made vehicle-info page dynamic

--- frontend/script/vehicle-info.js
+++ frontend/script/vehicle-info.js
@@ -2,11 +2,11 @@ const baseUrl='https://imperial-nerti-prabhakar-06383824.koyeb.app/api/v1/';
 
 
 const urlParams = new URLSearchParams(window.location.search);
-  const carId = urlParams.get('id');
+const carId = urlParams.get('id');
 
-  console.log("Car ID:", carId);
+console.log("Car ID:", carId);
 
-  let car;
+const car=fetchCar();
 
   async function fetchCar() {
     console.log("Fetching car data");
@@ -23,15 +23,16 @@ const urlParams = new URLSearchParams(window.location.search);
         throw new Error('Data fetch failed');
       }
 
-       car = data.content;
+       let car = data.content;
        console.log("Car data:", car);
        renderImages(car.imagesUrl);
+       renderRent(car.pricePerDay);
+       renderSpecs(car);
+       return car;
     } catch (error) {
       console.error('Error fetching car data:', error);
     }
   }
-
-  fetchCar();
 
   function renderImages(imagesUrls){   
     console.log("Rendering images"); 
@@ -47,4 +48,46 @@ const urlParams = new URLSearchParams(window.location.search);
     });
   }
 
-  
+  function renderRent(rent){
+    const priceElement=document.querySelector('.price-value');
+    priceElement.textContent=rent;
+    console.log(rent);
+  }
+
+  function renderSpecs(car){
+    const specsContainer=document.querySelector('.specs-container');
+    specsContainer.innerHTML='';
+    specsContainer.innerHTML=`<h1 class="text-white text-2xl sm:text-3xl md:text-4xl font-bold text-center mt-4 border-b-2 border-red-500 w-fit mx-auto px-4"><span>${(car.brand + ' ' + car.model).toUpperCase()}</span> SPECIFICATIONS</h1></h1>`;
+    for(const key in car.specs){
+      if(Object.hasOwnProperty.call(car.specs,key)){
+        const specsValue=car.specs[key];
+        if(typeof specsValue === 'object' && specsValue != null){
+          const specsSection=document.createElement('section');
+          specsSection.classList.add('specs-section', 'bg-[#373737]', 'text-white', 'px-[5%]', 'py-4', 'my-[10px]', 'mx-[10px]', 'rounded-md');
+          specsSection.innerHTML=`<h2 class="text-xl sm:text-2xl font-bold mb-4 text-red-500">${capitalizeFirstLetter(key)}</h2>`;
+          const nestedSpecsHtml=renderNestedSpecs(specsValue);
+          const specsHtml=`<div class="specs-container flex flex-col gap-3 sm:gap-4">${nestedSpecsHtml}</div>`;
+          specsSection.innerHTML+=specsHtml;
+          specsContainer.appendChild(specsSection);
+        }  
+      }
+    }
+  }
+
+  function renderNestedSpecs(nestedSpecs){
+    let nestedSpecsHtml='';
+    for(const key in nestedSpecs){
+      if(Object.hasOwnProperty.call(nestedSpecs,key)){
+        const nestedSpecsValue=nestedSpecs[key];
+        nestedSpecsHtml+=` <div class="specs-card flex flex-col sm:flex-row justify-between items-start sm:items-center border-b-1 border-b-white p-1 gap-1 sm:gap-0">
+            <h3 class="text-lg sm:text-xl text-gray-400">${key}</h3>
+            <p class="text-black text-base sm:text-lg font-semibold">${nestedSpecsValue}</p>
+          </div>`
+      }
+    }
+    return nestedSpecsHtml;
+  }
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
