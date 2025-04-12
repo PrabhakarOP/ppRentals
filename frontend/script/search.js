030b5a210b8648842fa69459f6ac69a08ee96d81
Prince Prabhakar <playhorn.pp@gmail.com>
2025-04-12 15:16:11 +0530

    added search popup overlay

--- frontend/script/search.js
+++ frontend/script/search.js
@@ -1,7 +1,58 @@
-const searchBar=document.getElementById('search-bar');
+const searchInput = document.getElementById('search-input');
+const searchBar = document.getElementById('search-bar');
+const searchResultsPopup = document.getElementById('search-results-popup');
+const searchResultsContainer = document.getElementById('search-results');
 
-searchBar.addEventListener("keydown",(event)=>{
+searchInput.addEventListener("keydown",(event)=>{
     if(event.key === "Enter"){
-        window.location.href='../html/search-results.html';
+        const searchTerm = searchInput.value.trim();
+        if (searchTerm) {
+            console.log("serachterm: "+searchTerm);
+            searchCars(searchTerm);
+        } 
     }
-})
+});
+
+
+async function searchCars(searchTerm) {
+    try {
+        const response = await fetch(`${baseUrl}car/search?query=${searchTerm}`);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        if (data.failure) {
+            throw new Error(data.error.errorMessage);
+        }
+        const searchResults = data.content;
+        renderSearchResults(searchResults);
+    } catch (error) {
+        console.error('Error searching cars:', error);
+    }
+}
+
+function renderSearchResults(searchResults) {
+    searchResultsContainer.innerHTML = '';
+    searchResults.forEach((result) => {
+        const resultHTML = `
+            <a href="/html/vehicle-info.html?id=${result.id}" class="search-result py-2 border-b border-gray-200 last:border-b-0">
+            <div class="search-result py-2 border-b border-gray-200 last:border-b-0">
+                <h2 class="text-lg font-bold">${result.model}</h2>
+                <p class="text-sm text-gray-600">Rs-${result.pricePerDay}/day</p>
+            </div>
+            </a>
+        `;
+        searchResultsContainer.insertAdjacentHTML('beforeend', resultHTML);
+    });
+    // Show the popup container
+    searchResultsPopup.classList.remove('hidden');
+}
+
+const closeButton = document.getElementById('close-button');
+closeButton.addEventListener('click', () => {
+    searchResultsPopup.classList.add('hidden');
+});
